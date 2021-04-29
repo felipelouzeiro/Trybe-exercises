b9b28c6e5a1c0378ee933595114ca6f238dd6ffe
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-04-29 15:02:11 -0300

    Exercicios 1 e 2

---
+++ exercises/bloco_4/dia_2/script.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//#1
+for (let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index]);
+};
+
+//#2
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1){
+    soma += numbers[index];
+} console.log(soma);
+
