1a7c80d1bb44a338e78730058c544f755b32db84
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-05-01 09:06:50 -0300

    Resolve exercicio 3

--- exercises/bloco_4/dia_3/script.js
+++ exercises/bloco_4/dia_3/script.js
@@ -21,3 +21,27 @@ let Ast = "*";
      linhaAst += Ast;
 };
 
+//3
+let n = 5;
+let Ast = '*';
+let space = "";
+let posicao = n;
+
+for (let linhaIndex = 0; linhaIndex < n; linhaIndex += 1) {
+  for (let colunaIndex = 0; colunaIndex <= n; colunaIndex += 1) {
+    if (colunaIndex < posicao) {
+      space += ' ';
+    } else {
+      space += Ast;
+    }
+  }
+  console.log(space);
+  space = '';
+  posicao -= 1;
+};
+
+
+
+//4
+
+
