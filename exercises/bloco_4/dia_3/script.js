e0986678fd214b56f3a24baff5098162dce64eff
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-05-01 09:05:56 -0300

    Resolve exercicios 1 e 2

---
+++ exercises/bloco_4/dia_3/script.js
@@ -0,0 +1,23 @@
+// 1
+let n = 5;
+let linhaAst = " ";
+
+let Ast = "*";
+ for(let index = 0; index < n; index += 1){
+     linhaAst += Ast;
+}
+
+for(let indexlinha = 0; indexlinha < n; indexlinha += 1){
+    console.log(linhaAst)
+};
+
+//2
+let n = 5;
+let linhaAst = " ";
+let Ast = "*";
+
+ for(let index = 0; index <= n; index += 1){
+     console.log(linhaAst);
+     linhaAst += Ast;
+};
+
