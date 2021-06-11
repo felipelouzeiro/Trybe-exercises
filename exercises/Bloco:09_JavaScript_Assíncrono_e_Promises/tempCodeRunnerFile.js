96a8cf788888e74a48e22b5c67be88eb65762008
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-06-11 08:54:08 -0300

    Exercicios b9.1

--- exercises/Bloco:09_JavaScript_Assíncrono_e_Promises/tempCodeRunnerFile.js
+++ exercises/Bloco:09_JavaScript_Assíncrono_e_Promises/tempCodeRunnerFile.js
@@ -1,29 +1,31 @@
-const messageDelay = () => Math.floor(Math.random() * 5000);
 
-const getMarsTemperature = () => {
-  const maxTemperature = 58;
-  return Math.floor(Math.random() * maxTemperature);
-}
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
-const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
-const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
-const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
 
-const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// definição da função sendMarsTemperature...
-const sendMarsTemperature = (callback) => {
-if (callback) { 
-  const temperatura = getMarsTemperature();
-  setTimeout(() => callback(temperatura), messageDelay());
-} else {
-  const error = 'Error getting temperature: Robot is busy';
-  setTimeout(() => console.log(error), messageDelay);
-}
-}; 
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(temperatureInFahrenheit, handleError);
-
-// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(handleError);
+console.log(planetDistanceFromSun(mars)); // A 1
+console.log(planetDistanceFromSun(venus)); // B 2
+console.log(planetDistanceFromSun(jupiter)); // C 3
